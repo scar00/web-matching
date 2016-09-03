5d2fbdba0442e00eaa3bd8059774a63d54bc8232
scar00 <scarlettrmcclure@gmail.com>
2016-09-03 14:34:25 +1000

    Began building site from template

---
+++ web-matching/web-matching/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(web_matching.Startup))]
+namespace web_matching
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
